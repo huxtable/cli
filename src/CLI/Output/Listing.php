a28d8dc5115b4189b2edfbff5145a59b2c1ed355
Ashur Cabrera <hello@cabreramade.co>
2016-01-01 20:01:50 -0800

    dynamic column layout

--- src/CLI/Output/Listing.php
+++ src/CLI/Output/Listing.php
@@ -5,7 +5,7 @@
  */
 namespace Huxtable\CLI\Output;
 
-class Listing
+class Listing extends \Huxtable\CLI\Output
 {
 	/**
 	 * @var	array
@@ -22,12 +22,10 @@ class Listing
 	}
 
 	/**
-	 * @param
 	 * @return	string
 	 */
 	public function flush()
 	{
-		$columns = 2;
 		$output  = '';
 
 		// Determine padding
@@ -39,27 +37,42 @@ class Listing
 				$maxLength = strlen( $item );
 			}
 		}
-		$padding = $maxLength + 4;
 
-		// Render each line
-		$indexMid   = ceil( count( $this->items ) / 2 );
-		$indexLeft  = 0;
-		$indexRight = $indexMid;
+		$padding = $maxLength + 4;
+		$width = $this->getCols();
+
+		$cols = floor( $width / $padding );
+		$rows = ceil( count( $this->items ) / $cols );
 
 		if( count( $this->items ) == 0 )
 		{
 			return;
 		}
 
-		while( isset( $this->items[$indexLeft] ) && $indexLeft < $indexMid )
+		if( $rows == 1 )
 		{
-			$itemLeft  = sprintf( "%-{$padding}s", $this->items[$indexLeft] );
-			$itemRight = isset( $this->items[$indexRight] ) ? sprintf( "%-{$padding}s", $this->items[$indexRight] ) : '';
+			foreach( $this->items as $item )
+			{
+				$items[] = sprintf( "%-{$padding}s", $item );
+			}
 
-			$output .= "{$itemLeft}{$itemRight}\n";
+			return implode( '', $items );
+		}
 
-			$indexLeft++;
-			$indexRight++;
+		for( $r = 0; $r < $rows; $r++ )
+		{
+			$line = '';
+
+			for( $c = 0; $c < $cols; $c++ )
+			{
+				$index = $r + ($c * $rows);
+				if( isset( $this->items[$index] ) )
+				{
+					$line .= sprintf( "%-{$padding}s", $this->items[$index] );
+				}
+			}
+
+			$output .= $line . PHP_EOL;
 		}
 
 		return $output;
